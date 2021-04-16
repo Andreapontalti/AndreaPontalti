b0891d42964ebc0ff35296b9c55229d18b3793f6
Andreapontalti <80822673+Andreapontalti@users.noreply.github.com>
2021-04-16 09:49:21 +0200

    Update R code knitr.r

--- R code knitr.r
+++ R code knitr.r
@@ -4,3 +4,7 @@
 setwd("C:/lab/")
 install.packages("knitr")   ##pacchetto importante da scaricare
 library(knitr)
+
+
+stitch(R_code_greenland.r), template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+  ####ancora una volta Andre hai perso il filo del discorso e ti dovrai sorbire n'altra ora e mezza di lezione per fare sto scempio. RIP
