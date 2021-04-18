4c49bbef154da54c9800e9b3ea5989b241be52c2
Andreapontalti <80822673+Andreapontalti@users.noreply.github.com>
2021-04-18 18:58:09 +0200

    Update R code knitr.r

--- R code knitr.r
+++ R code knitr.r
@@ -1,17 +1,16 @@
 #
 
 
-setwd("C:/lab/")
+setwd("C:/lab/")   #vuol dire SET WORKING DIRECTORY
 install.packages("knitr")   ##pacchetto importante da scaricare
+
 library(knitr)
 library(raster)
 library(rasterVis)    ###apriamo varie librerie a caso
 setwd("C:/lab/greenland")
 
-lst_2000 <- raster("lst_2000.tif") 
-lst_2005 <- raster("lst_2005.tif") 
-lst_2010 <- raster("lst_2010.tif")
-lst_2015 <- raster("lst_2015.tif")  
+require(knitr)
 
-stitch(R_code_greenland.r), template=system.file("misc", "knitr-template.Rnw", package="knitr"))
-  ####ancora una volta Andre hai perso il filo del discorso e ti dovrai sorbire n'altra ora e mezza di lezione per fare sto scempio. RIP
+stitch("R_code_greenland.tex.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))   #crea automaticamente un report basato su uno script di R e un template
+  ####ancora una volta Andre hai perso il filo del discorso e ti dovrai sorbire n'altra ora e mezza di lezione per fare sto scempio. RIP ###ORA VA ANDRE ORA VAAAA IL PROBLEMA ERA UNA ROBA OSCENA CHE TIPO IL FILE ERA UN .TXT DI BASE E NON AVEVI INSERITO QUELLA COSA ALEEEEEEEEEE
+
